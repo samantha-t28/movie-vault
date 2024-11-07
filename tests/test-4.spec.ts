14eeffe2346b1194bda330f9ebd7f15658f388de
samantha-t28 <samanthatan228@gmail.com>
2024-11-07 13:03:01 -0800

    remove test recording file

--- tests/test-4.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('should switch to dark mode when theme is toggled', async ({ page }) => {
-    await page.goto('http://localhost:5173/');
-    await page.getByRole('img', { name: 'Moon icon' }).click();
-    await expect(page.locator('body')).toHaveClass(/theme--dark/);
-});
