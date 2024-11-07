b5be5cf9eccd97c9b7fdd6d4e5bd14aa04a5602a
samantha-t28 <samanthatan228@gmail.com>
2024-11-07 00:45:55 -0800

    test: updated search results test
    - make search results test adaptable to API updates by removing hardcoded count

--- tests/search-page.spec.js
+++ tests/search-page.spec.js
@@ -1,15 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('should display search results for "James Bond"', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173/');
+});
+
+test('should display search results for "James Bond"', async ({ page }) => {
     await page.getByPlaceholder('Search for movies or actors').click();
-    // await page.goto('http://localhost:5173/?movie=james+bon');
     await page
         .getByPlaceholder('Search for movies or actors')
         .fill('james bond');
-    // await page.goto('http://localhost:5173/?movie=james+bond');
     await page.getByPlaceholder('Search for movies or actors').press('Enter');
-    await expect(page.locator('h2')).toContainText(
-        'Displaying 83 Results for "james bond"'
-    );
+    await expect(page.locator('h2')).toContainText('Results for "james bond"');
 });
