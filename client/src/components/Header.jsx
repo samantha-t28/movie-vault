ac0c880abec539d239e134f4e99f9e91708afb42
samantha-t28 <samanthatan228@gmail.com>
2024-11-06 17:18:10 -0800

    clean up theme class in Header and reorganize search test files
    * remove hardcoded "theme--light" class from Header.jsx
    * move search movie test from homepage.spec.js to searchpage.spec.js

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,33 +7,31 @@ export const Header = ({ onSearch }) => {
     const { theme } = useTheme();
 
     return (
-        <div className="theme--light">
-            <div className="header-wrapper">
-                <header className="header">
-                    <Link to="/">
-                        <img
-                            id="logo"
-                            className="header__logo-img"
-                            src={
-                                theme === 'light'
-                                    ? '/movie-vault-logo.svg'
-                                    : '/movie-vault-logo-white.svg'
-                            }
-                            alt="Movie Vault logo"
-                        />
-                    </Link>
-                    <div className="header__search-bar">
-                        <SearchBar onSearch={onSearch} />
-                    </div>
-                    <div className="header__nav-icons">
-                        <i className="fas fa-heart heart-icon"></i>
-                        <ThemeToggle />
-                    </div>
-                    <button className="hamburger-menu">
-                        <i className="fas fa-bars"></i>
-                    </button>
-                </header>
-            </div>
+        <div className="header-wrapper">
+            <header className="header">
+                <Link to="/">
+                    <img
+                        id="logo"
+                        className="header__logo-img"
+                        src={
+                            theme === 'light'
+                                ? '/movie-vault-logo.svg'
+                                : '/movie-vault-logo-white.svg'
+                        }
+                        alt="Movie Vault logo"
+                    />
+                </Link>
+                <div className="header__search-bar">
+                    <SearchBar onSearch={onSearch} />
+                </div>
+                <div className="header__nav-icons">
+                    <i className="fas fa-heart heart-icon"></i>
+                    <ThemeToggle />
+                </div>
+                <button className="hamburger-menu">
+                    <i className="fas fa-bars"></i>
+                </button>
+            </header>
         </div>
     );
 };
